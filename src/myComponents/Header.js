7e34f27382e0253dbe39b025539a301b1f54f382
Paracetamol77 <joshikamal134@gmail.com>
2024-11-28 09:46:03 +0545

    first commit in frontend

---
+++ src/myComponents/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>ultra fast citizenship</div>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li><a href="#home" className={styles.navLink}>Home</a></li>
+          <li><a href="#about" className={styles.navLink}>About</a></li>
+          <li><a href="#services" className={styles.navLink}>Services</a></li>
+          <li><a href="#contact" className={styles.navLink}>Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
