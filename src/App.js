60118222a595857837f1eb6a30f3036fc19442f9
Paracetamol77 <joshikamal134@gmail.com>
2024-11-29 22:02:38 +0545

    finalpush

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HeroSection from './myComponents/heroSection';
-import Signin from './myComponents/Signinpage';
 import Client from './myComponents/Client';
 import NIDVerification from './myComponents/NIDVerification';
 function App() {
@@ -12,10 +11,8 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<HeroSection />} />
-        <Route path="/signup-page" element={<Signin/>} />
-        <Route path="/signup" element={<Signin/>}/>
-        <Route path="Client" element={<Client/>}/>
-        <Route path="/" element={<Client />} />
+        <Route path="/Client" element={<Client/>} />
+        <Route path="/" element={<Client/>}/>
         <Route path="/nidverification" element={<NIDVerification />} />
       </Routes>
     </Router>
--- src/myComponents/heroSection.js
+++ src/myComponents/heroSection.js
@@ -6,13 +6,13 @@ const HeroSection = () => {
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    navigate('/signup-page'); // Navigate to the next page
+    navigate('/client'); // Navigate to the next page
   };
 
   return (
     <div className={styles.heroContainer}>
       <div className={styles.heroContent}>
-        <h1 className={styles.heroHeading}>"Book Your Tickets Now"</h1>
+        <h1 className={styles.heroHeading}>"Book Your Token Now"</h1>
         <p className={styles.heroSubheading}>"सरकारी कार्यालयहरूको लागि टिकटहरू छनौट गर्नुहोस्"</p>
         <button className={styles.welcomeButton} onClick={handleButtonClick}>
           Get Started
